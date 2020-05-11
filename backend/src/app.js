8fa9fdf2b1f46362e9ee75dbed9091c3cafc763d
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-05-11 14:27:49 -0300

    CORS origin added

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,9 @@ app.use(Sentry.Handlers.requestHandler());
 
 app.use(helmet());
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+}));
 
 app.use(express.json());
 app.use(rateLimitConfig);
