e609e8865c99d122bf5e68867380ff37665fc89c
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-05-20 16:10:32 -0300

    Fixed smtp secure

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_SECURE,
+  secure: process.env.SMTP_SECURE === 'true',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
