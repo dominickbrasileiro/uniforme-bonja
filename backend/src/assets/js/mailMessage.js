088f2353f6296896a17dd6e41d67d3454f7615a8
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-05-25 20:50:29 -0300

    Mail messages updated

--- backend/src/assets/js/mailMessage.js
+++ backend/src/assets/js/mailMessage.js
@@ -3,7 +3,7 @@ module.exports = ({
 }) => `
 <div style="color: #000 !important; font-family: Arial, Helvetica, sans-serif;">
   <h3>Bem-vindo(a), ${name}!</h3>
-  <p>Essa é a sua chave de acesso ao portal de camisetas do Terceirão Bonja 2020: <h4>${access_pin}</h4></p>
+  <p>Essa é a sua chave de acesso ao Portal Terceirão Bonja 2020: <h4>${access_pin}</h4></p>
   <p>Ela será a sua única forma de acesso ao portal, anote com cuidado!</p>
       <p style="font-weight: bold; font-size: 16px"> >>> <a href="${frontendUrl}/#/login?email=${email}" target="_blank">Acessar o portal</a> </p>
       <br />
--- backend/src/assets/js/recoverMailMessage.js
+++ backend/src/assets/js/recoverMailMessage.js
@@ -4,7 +4,7 @@ module.exports = ({
 <div style="color: #000 !important; font-family: Arial, Helvetica, sans-serif;">
   <h3>Olá, ${name}!</h3>
   <p>Você solicitou a recuperação da sua chave de acesso.</p>
-  <p>Essa é a sua chave de acesso ao portal de camisetas do Terceirão Bonja 2020: <h4>${access_pin}</h4></p>
+  <p>Essa é a sua chave de acesso ao Portal Terceirão Bonja 2020: <h4>${access_pin}</h4></p>
   <p>Ela será a sua única forma de acesso ao portal, anote com cuidado!</p>
       <p style="font-weight: bold; font-size: 16px"> >>> <a href="${frontendUrl}/#/login?email=${email}" target="_blank">Acessar o portal</a> </p>
       <br />
